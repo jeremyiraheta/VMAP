dc68e51ddc55d69988b846b00330a1f31341b5e7
jeremyiraheta <47946244+jeremyiraheta@users.noreply.github.com>
2020-04-21 22:17:49 -0600

    Initial commit

---
+++ app/src/main/java/com/utec/vmap/ui/home/HomeViewModel.java
@@ -0,0 +1,19 @@
+package com.utec.vmap.ui.home;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class HomeViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public HomeViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("Zona del mapa");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
